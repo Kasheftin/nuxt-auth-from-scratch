01fab58a6107c16c3ff2d07b118c5491cd26242e
Kasheftin <Kasheftin@gmail.com>
2018-01-23 03:03:43 +0200

    #7 Step: working with the cookies on server side

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,3 +1,6 @@
+import cookie from 'cookie'
+import {setAuthToken, resetAuthToken} from '~/utils/auth'
+
 export const state = () => ({
   sidebar: false
 })
@@ -5,5 +8,27 @@ export const state = () => ({
 export const mutations = {
   toggleSidebar (state) {
     state.sidebar = !state.sidebar
+  }
+}
+
+export const actions = {
+  nuxtServerInit ({dispatch}, context) {
+    return new Promise((resolve, reject) => {
+      const cookies = cookie.parse(context.req.headers.cookie || '')
+      if (cookies.hasOwnProperty('x-access-token')) {
+        setAuthToken(cookies['x-access-token'])
+        dispatch('auth/fetch')
+          .then(result => {
+            resolve(true)
+          })
+          .catch(error => {
+            resetAuthToken()
+            resolve(false)
+          })
+      } else {
+        resetAuthToken()
+        resolve(false)
+      }
+    })
   }
 }
